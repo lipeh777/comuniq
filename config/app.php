aab6afcb693a7d6f1ead57f8d38256486540fa00
AlissonThyago <alissonthiagoamorim@gmail.com>
2019-09-23 14:50:16 -0300

    GH-6 Adicionando arquivos para implementação da pagina de cadastro.

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Appzcoder\CrudGenerador\CrudGeneradorServiceProvider::class,
+        // Collective\Html\HtmlServiceProvider::class,
+
     ],
 
     /*
@@ -225,6 +228,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Form' => Collective\Html\FormFacade::class,
+        'HTML' => Collective\Html\HtmlFacade::class,
 
     ],
 
