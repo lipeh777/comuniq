a47fc333cc67c1be95c686525f85285d3deaaa74
AlissonThyago <alissonthiagoamorim@gmail.com>
2019-09-30 17:54:23 -0300

    GH-27 estilizando a pagina de cadastro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,20 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Cadastre-se') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+<div class="container center">
+    <div class="container">
+        <div class="card">
+            <div class="row center">
+                <div class="card-header center"><h5>{{ __('Cadastre-se') }}</h5></div>
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
+                <form class="col s12" method="POST" action="{{ route('register') }}">
+                    @csrf
 
-                            <div class="col-md-6">
+                    <div class="row">
+                        <div class="container">
+                            <div class="input-field col s12">
+                                <i class="material-icons prefix" id="icone">short_text</i>
+                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
+
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
@@ -24,11 +26,14 @@
                                 @enderror
                             </div>
                         </div>
+                    </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
+                    <div class="row">
+                        <div class="container">
+                            <div class="input-field col s12">
+                                <i class="material-icons prefix" id="icone">email</i>
+                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
 
-                            <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
@@ -38,11 +43,14 @@
                                 @enderror
                             </div>
                         </div>
+                    </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Senha') }}</label>
+                    <div class="row">
+                        <div class="container">
+                            <div class="input-field col s12">
+                                <i class="material-icons prefix" id="icone">vpn_key</i>
+                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Senha') }}</label>
 
-                            <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
@@ -52,24 +60,27 @@
                                 @enderror
                             </div>
                         </div>
+                    </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirme a Senha') }}</label>
+                    <div class="row">
+                        <div class="container">
+                            <div class="input-field col s12">
+                                <i class="material-icons prefix" id="icone">lock</i>
+                                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirme a Senha') }}</label>
 
-                            <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
+                    </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Cadastre-se') }}
-                                </button>
-                            </div>
+                    <div class="form-group row mb-0">
+                        <div class="col-md-6 offset-md-4">
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('Cadastre-se') }}
+                            </button>
                         </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
