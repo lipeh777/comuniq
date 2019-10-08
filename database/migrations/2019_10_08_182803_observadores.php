5d43912f5993219f7629efa13c5b5b898c0ed3fa
JonissonGomes <joni_gomes@hotmail.com>
2019-10-08 15:56:58 -0300

    GH-36 migrations da tabela Observadores

---
+++ database/migrations/2019_10_08_182803_observadores.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Observadores extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('observadores', function (Blueprint $table)
+            $table->bigIncremenst('id');
+            $table->foreign('usuario_id')->references('id')->on('observadores');
+            $table->foreign('chamados_id')->references('id')->on('observadores');
+            $table->timestamps();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+       Schema::dropIfExists('observadores');
+    }
+}
