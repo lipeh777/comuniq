b991310f88f662c8afc7e466ef344868468a9451
filipeandrev <filipe.andrev7@gmail.com>
2019-10-12 13:29:39 -0300

    GH-38 Adicionando a migration referente a model de chamado

---
+++ database/migrations/2019_10_12_145814_create_chamados_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChamadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chamados', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->primary('chamados_id');
+            $table->string('descricao', 255)->nullable();//nullable indica que o campo é opcional.
+            $table->timestamps();
+            $table->integer('fk_usuario_id');
+            $table->foreign('fk_usuario_id')->references('usuario_id')->on('usuarios')->unsigned();
+            $table->integer('fk_itens_de_patrimonio_id');
+            $table->foreign('fk_itens_de_patrimonio_id')->references('itens_de_patrimônio_id')->on('itenspatrimonio')->unsigned();
+            // $table->integer('chamado_pai');
+            // $table->foreign('chamado_pai')->references('chamado_pai')->on('chamados');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chamados');
+    }
+}
