9f48ca242bb1a66a82a5642801e4d1313c02f56f
filipeandrev <filipe.andrev7@gmail.com>
2019-10-14 19:26:05 -0300

    GH-38 Alterando nome da fk, fk_observadores_id para fk_users_id e alterando tabela de observadores para users

--- database/migrations/2019_10_12_145814_create_chamados_table.php
+++ database/migrations/2019_10_12_145814_create_chamados_table.php
@@ -16,10 +16,10 @@ class CreateChamadosTable extends Migration
         Schema::create('chamados', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('descricao', 255)->nullable();//nullable indica que o campo é opcional.
-            $table->unsignedBigInteger('fk_observadores_id');
+            $table->unsignedBigInteger('fk_users_id');
             $table->unsignedBigInteger('fk_statuses_id');
             $table->timestamps();
-            $table->foreign('fk_observadores_id')->references('id')->on('observadores');
+            $table->foreign('fk_users_id')->references('id')->on('users');
             $table->foreign('fk_statuses_id')->references('id')->on('statuses');
         });
     }
