90c140f8ac0e63110cb6ebb42fcada2321028629
k0rgana <anagrom1999@gmail.com>
2019-10-15 18:19:22 -0300

    GH-60 adicionando seed de usuario

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+		for ($i = 0; $i < 5; $i++) {
+			User::create([
+				'name' => $faker->name,
+				'email' => $faker->email,
+				'password' => bcrypt($faker->word),
+			]);
+		}
+    }
+}
