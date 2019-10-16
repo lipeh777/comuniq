105c284bff0fa6ea92c53f1ed6f856e9ea690765
filipeandrev <filipe.andrev7@gmail.com>
2019-10-16 13:15:37 -0300

    GH-39 Preenchendo model de ItenPatrimonio com campos da tabela presente na migration iten_patrimonios

--- app/ItenPatrimonio.php
+++ app/ItenPatrimonio.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ItenPatrimonio extends Model
 {
+    protected $fillable = ['numero_tombamento'];
+    protected $guarded = ['id', 'fk_instituicoes_id'];
+    protected $table = 'iten_patrimonios';
     //
 }
