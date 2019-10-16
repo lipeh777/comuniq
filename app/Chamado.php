a963a96d39352bbb48fb4e77bdd0ee0c2fa221e5
filipeandrev <filipe.andrev7@gmail.com>
2019-10-16 12:37:56 -0300

    GH-38 Preenchendo model de Chamado com os campos presentes na tabela da migration

--- app/Chamado.php
+++ app/Chamado.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chamado extends Model
 {
+    protected $fillable = ['descricao'];
+    protected $guarded = ['id', 'fk_users_id', 'fk_statuses_id'];
+    protected $table = 'chamados';
     //
 }
