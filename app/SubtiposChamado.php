ad63e117f13c17c44aaf8f74d945c01363138e2f
filipeandrev <filipe.andrev7@gmail.com>
2019-10-17 17:30:14 -0300

    GH-44 Preenchendo model de SubtiposChamado

--- app/SubtiposChamado.php
+++ app/SubtiposChamado.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class SubtiposChamado extends Model
 {
+	protected $fillable = ['descricao'];
+	protected $guarded = ['id'];
+	protected $table = 'subtipos_chamados';
     //
 }
