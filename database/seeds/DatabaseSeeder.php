19a9b3239d65925bec17b7ba470d5387337e026c
leonardolfp <leonardolfp15@gmail.com>
2019-10-22 16:49:42 -0300

    GH-58 adicionando o chamado da tabela Chamado_Tipos aos seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,5 +19,6 @@ class DatabaseSeeder extends Seeder
         $this->call(SetoresTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(ItemPatrimonioTableSeeder::class);
+        $this->call(ChamadoTipoTableSeeder::class);
     }
 }
