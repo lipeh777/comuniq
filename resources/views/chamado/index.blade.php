dacb647e3ef1414be50442c7aa7667462ef06e43
AlissonThyago <alissonthiagoamorim@gmail.com>
2019-10-26 22:11:50 -0300

    GH-56 adicionando sistema de mensagem de erro/sucesso, quando for criado um novo chamado

--- resources/views/chamado/index.blade.php
+++ resources/views/chamado/index.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
 
+  @if ($message = Session::get('message'))
+      <div class="alert alert-success">
+        <p>{{ $message }}</p>
+      </div>
+  @endif
+
   <div class="row">
     <div class="col-md-6 col-md-offset-3">
       {{ Form::open(['route'=>'chamado.store', 'method'=>'POST']) }}
