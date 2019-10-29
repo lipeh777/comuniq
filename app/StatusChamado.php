3613af1a923610a628ee834c670539440ae7c343
perolasanja <peroladiniz2013@gmail.com>
2019-10-29 15:34:25 -0300

    GH-41 readicionando model para a tabela StatusChamado

---
+++ app/StatusChamado.php
@@ -0,0 +1,14 @@
+<?php
+
+ namespace App;
+
+ use Illuminate\Database\Eloquent\Model;
+
+ class StatusChamado extends Model
+ {
+ 	public $table = 'status_chamados';
+ 	
+     protected $fillable = [
+         'comentario'
+     ];
+ }
