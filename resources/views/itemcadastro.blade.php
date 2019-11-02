1d59e0dc8ed89cac1507d4d0cc72891f438baea3
filipeandrev <filipe.andrev7@gmail.com>
2019-11-02 14:03:21 -0300

    GH-11 Movendo formulario da view itemcadastro.blade.php para a create.blade.php

--- resources/views/itemcadastro.blade.php
+++ resources/views/itemcadastro.blade.php
@@ -7,23 +7,7 @@
     <div class="card darken-1">
       <div class="card-header center"><h5>{{ __('Cadastro de Itens') }}</h5></div>
       <div class="card-content">
-        <div class="row">
-
-    <!--  -->
-
-    <form method="post" action="{{ route('itemcadastro.store') }}"> <!-- {{url('/create/ticket')}} -->
-        <div class="form-group">
-            <input type="hidden" value="{{csrf_token()}}" name="_token" />
-            <div class="input-field col s12">
-                                <i class="material-icons prefix" id="icone">border_color</i>
-            <label for="title">Número de Tombamento</label>
-            <input type="text" class="form-control" name="title"/>
-        </div>
-       
-        <button type="submit" class="btn btn-primary green accent-4">Cadastre</button>
-        </form>
-
-</div>
+ <!-- @yield('form') -->
 
 </div>
       </div>
