21fcbffd1bb5a8d30fc9d8e27fedfa1d3e94297a
AlissonThyago <alissonthiagoamorim@gmail.com>
2019-11-02 16:46:33 -0300

    GH-64 chamados são listadoso com base no id de cada usuário

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Chamado;
+use App\User;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -22,7 +25,8 @@ class HomeController extends Controller
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
-    {
-        return view('home');
+    {   
+        $chamado = Chamado::where('fk_users_id', auth()->user()->id)->get();
+        return view('home')->with(['chamado' => $chamado]);
     }
 }
