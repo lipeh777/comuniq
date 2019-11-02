49504eef8341894f2a678d5e96e89c5114432b35
AlissonThyago <alissonthiagoamorim@gmail.com>
2019-11-02 16:58:25 -0300

    GH-56 adicionando a lógica de manipulação do ChamadoController

--- app/Http/Controllers/ChamadoController.php
+++ app/Http/Controllers/ChamadoController.php
@@ -3,34 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use app\Chamado;
+use App\Chamado;
 
 class ChamadoController extends Controller {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index(){
 
-        $chamado = Chamado::orderBy('created_at', 'description')->paginate(10);
+        $chamado = Chamado::all();
 
-        // return view('chamado.index',['chamado' => $chamado]);
+        return view('chamado.index',['chamado' => $chamado]);
     }
 
     public function create(){
 
-    	// return view('chamado.create');
+        //
 
     }
 
-	public function store(ChamadoRequest $request){
+    public function store(Request $request){
 
-	        $chamado = new Chamado;
-	        $chamado->description = $request->descricao;
-	        // $chamado->user = $request->users;
-	        // $chamado->observadores = $request->observadores;
-	        // $chamado->status = $request->status;
-	        $chamado->save();
-	        return redirect()->route('chamado.index')->with('message', 'Called created successfully!');
+        $tombamento = $request->get("tombamento");
+        // $chamado = new Chamado;
+        // $chamado->description = $request->descricao;
+        // $chamado->user = $request->users;
+        // $chamado->observadores = $request->observadores;
+        // $chamado->status = $request->status;
+        // $chamado->save();
+        $chamado = Chamado::where('descricao');
+            $this->validate($request,[
+              'descricao'=>'string|max:255',
+            ]);
+            Chamado::create($request->all());
+            return redirect()->route('chamado.index')->with('message','Chamado criado com sucesso');
 
-	    }
-	public function show($id){
+    }
+    public function show($id){
 
         //
     }
