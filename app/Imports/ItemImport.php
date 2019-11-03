fd890fd6d41fe2eed95e48f4b0bf63557c8d651f
k0rgana <anagrom1999@gmail.com>
2019-11-03 00:03:10 -0300

    GH-11 adicionando classe de importacao de itens

---
+++ app/Imports/ItemImport.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Imports;
+
+use App\Item;
+use Maatwebsite\Excel\Concerns\ToModel;
+
+class ItemImport implements ToModel, WithProgressBar
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        return new Item([
+            // 'campo'    => $row[indice], 
+            'numero_tombamento' => $row[0],
+        ]);
+    }
+}
